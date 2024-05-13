90001cc5bf245087a6c3b6b97e745430e90c7c15
NoraDjennadi <62260957+NoraDjennadi@users.noreply.github.com>
2024-05-13 17:28:08 +0200

    Refactor (#7)
    * Clean Code
    ---------
    
    Co-authored-by: ndjennadi <nora.djennadi@fr.transavia.com>

--- main.py
+++ main.py
@@ -9,24 +9,8 @@ import requests
 import cv2
 
 
-def clamp_detections(detections: List[DoggyOrder], limit: int = 5) -> List[DoggyOrder]:
-    """Clamp the number of detections to not exceed limit"""
-    while len(detections) > limit:
-        detections.pop(0)
-    return detections
-
-
-def get_order_given(last_detections: List[DoggyOrder]) -> DoggyOrder:
-    """Returns the order to give as regard of all the detections given"""
-    for order in DoggyOrder:
-        if all(order == detection for detection in last_detections):
-            return order
-    return DoggyOrder.NONE
-
-
 def main():
     # Init vars and load models here
-    last_detections = []
 
     if not doggy.start():
         raise RuntimeError("Doggy did not start!")
@@ -52,14 +36,9 @@ def main():
                 response = requests.post(
                     url_vm, files=files, headers=headers, timeout=5
                 )
-                name = response.json()["name"]
-                distance = response.json()["distance"]
                 current_order = 1 if response.json()["message"] == "Go" else -1
-                print(new_detection)
-                print(current_order)
 
                 if current_order != DoggyOrder.NONE and doggy.ready():
-                    last_detections = []
                     doggy.do(current_order)
             else:
                 print("I'll sleep to wait a little.")
